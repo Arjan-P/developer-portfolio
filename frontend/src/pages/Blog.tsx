e5815f111355ec70871e74e5bd4afa09cfdd3681
Arjan <arjanpingale@gmail.com>
2026-01-03 10:37:48 +0530

    Added hook to change page title according to route

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,5 +1,9 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 export default function BlogLayout() {
+  useEffect(() => {
+    document.title = "Blog";
+  }, []);
   return <Outlet />
 }
--- frontend/src/pages/admin/AdminLayout.tsx
+++ frontend/src/pages/admin/AdminLayout.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react"
 import { Link, Outlet } from "react-router-dom"
 
 export default function AdminLayout() {
+  useEffect(() => {
+    document.title = "Admin";
+  }, []);
   return (
     <div>
       <h1>Admin Panel</h1>
