8b1aa11fddc58e5d0775acca4ae66174a32f20da
Arjan <arjanpingale@gmail.com>
2026-01-31 23:52:34 +0530

    added gfm and rehype-raw extentions for markdown rendering

--- frontend/src/components/MarkdownRendered.tsx
+++ frontend/src/components/MarkdownRendered.tsx
@@ -1,9 +1,11 @@
 import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
 
 interface MarkdownRendererProp {
   markdown: string;
 }
 
 export default function MarkdownRendered({ markdown } : MarkdownRendererProp) {
-  return <ReactMarkdown>{markdown}</ReactMarkdown> 
+  return <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{markdown}</ReactMarkdown> 
 }
