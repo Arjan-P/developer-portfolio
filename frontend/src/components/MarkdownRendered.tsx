a32f90378492790254981bf96c95413245c8f203
Arjan <arjanpingale@gmail.com>
2026-02-01 00:09:25 +0530

    added image styling for images in markdown

--- frontend/src/components/MarkdownRendered.tsx
+++ frontend/src/components/MarkdownRendered.tsx
@@ -1,11 +1,16 @@
 import ReactMarkdown from 'react-markdown';
-import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 
 interface MarkdownRendererProp {
   markdown: string;
 }
 
-export default function MarkdownRendered({ markdown } : MarkdownRendererProp) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{markdown}</ReactMarkdown> 
+export default function MarkdownRendered({ markdown }: MarkdownRendererProp) {
+  return (
+    <div className='markdown'>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        {markdown}
+      </ReactMarkdown>
+    </div>
+  )
 }
